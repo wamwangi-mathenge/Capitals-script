0bff524f678da48900846208e2bb846987901b26
Brian <wamwangimathenge@gmail.com>
2023-09-25 09:51:17 +0300

    Create capitals shell script

---
+++ create-directory-structure.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+mkdir countries
+
+cd countries
+
+mkdir USA UK Kenya
+
+echo "Washington, D.C" > USA/capital.txt
+
+echo "London" > UK/capital.txt
+
+echo "Nairobi" > Kenya/capital.txt
+
+uptime
